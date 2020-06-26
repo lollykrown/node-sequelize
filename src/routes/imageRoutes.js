e67836d0a9efa4596a29502b1a23acbbf57c1933
lollykrown <joe_kayu@yahoo.com>
2020-06-26 12:59:01 +0100

    .

--- src/routes/imageRoutes.js
+++ src/routes/imageRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const homeController = require("./controllers/home");
-const uploadController = require("./controllers/upload");
-const upload = require("./middleware/upload");
+const homeController = require("../controllers/home");
+const uploadController = require("../controllers/upload");
+const upload = require("../middleware/upload");
 
 let routes = (app) => {
   router.get("/", homeController.getHome);
