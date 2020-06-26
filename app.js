f3b361c100ddfaeadce1ae7342f86d3dc8886066
lollykrown <joe_kayu@yahoo.com>
2020-06-26 11:31:55 +0100

    .

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const app = express()
 app.use(bodyParser.json())
 
 // API ENDPOINTS
+const { User, Blog, Tag } = require('./sequelize')
+// create a user
+app.post('/api/users', (req, res) => {
+    User.create(req.body)
+        .then(user => res.json(user))
+})
+// get all users
+app.get('/api/users', (req, res) => {
+    User.findAll().then(users => res.json(users))
+})
 
 const port = 3000
 app.listen(port, () => {
